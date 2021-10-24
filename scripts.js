f1466e455916cc6a6a1cbc3cdd1ffa95a2c3e5f1
Damian Sire <damiansire@hotmail.com>
2021-10-24 00:18:28 -0300

    Initial commit

--- scripts.js
+++ scripts.js
@@ -3,10 +3,20 @@ const dolarUruguayApi = "https://cotizaciones-brou.herokuapp.com/api/currency/la
 
 let dolarUy;
 
+let dolarBlueAr;
+
+let dolarTuristaAr;
+
+let dolarOficialAr;
+
+const cardContainer = document.getElementsByClassName("card-container")[0];
+
 class DolarUy {
     constructor(responseData) {
         this.sell = responseData.rates["USD"].sell;
         this.buy = responseData.rates["USD"].buy;
+        this.screenText = "Dolar Uruguay";
+        this.img = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/1200px-Flag_of_Uruguay.svg.png";
     }
 }
 
@@ -14,6 +24,7 @@ async function getDolarUyData() {
     const response = await fetch(dolarUruguayApi);
     const responseData = await response.json();
     dolarUy = new DolarUy(responseData);
+    renderDolar(dolarUy);
 }
 
 getDolarUyData();
@@ -27,6 +38,8 @@ class DolarBlueAr {
     constructor(dolarData) {
         this.sell = dolarData.venta;
         this.buy = dolarData.compra;
+        this.screenText = "Dolar Blue AR"
+        this.img = "https://www.andbank.es/observatoriodelinversor/wp-content/uploads/2014/01/DOLAR_blue.jpg";
     }
 }
 
@@ -34,6 +47,8 @@ class DolarTuristaAr {
     constructor(dolarData) {
         this.sell = dolarData.venta;
         this.buy = dolarData.compra;
+        this.screenText = "Dolar Turista AR"
+        this.img = "https://www.cronista.com/files/image/127/127578/5ff77245dc84c.jpg";
     }
 }
 
@@ -41,12 +56,10 @@ class DolarOficialAr {
     constructor(dolarData) {
         this.sell = dolarData.venta;
         this.buy = dolarData.compra;
+        this.screenText = "Dolar Oficial AR"
+        this.img = "https://resizer.iproimg.com/unsafe/880x/filters:format(webp)/https://assets.iprofesional.com/assets/jpg/2020/08/501745.jpg?7.2.4";
     }
 }
-
-let dolarBlueAr;
-let dolarTuristaAr;
-let dolarOficialAr;
 
 async function getDolarArData() {
     const response = await fetch(dolarArgentinaApi);
@@ -55,6 +68,9 @@ async function getDolarArData() {
     const filtredDolarData = dolarData.filter(dolar => ['Dolar Blue', 'Dolar turista', 'Dolar Oficial'].includes(dolar.nombre))
     console.log(filtredDolarData);
     saveDolarArData(filtredDolarData);
+    renderDolar(dolarBlueAr);
+    renderDolar(dolarTuristaAr);
+    renderDolar(dolarOficialAr);
 }
 
 function saveDolarArData(filtredDolarData) {
@@ -69,3 +85,21 @@ function saveDolarArData(filtredDolarData) {
 
 getDolarArData();
 
+//Renderizado
+
+function getDolarHtml(dolar) {
+    return `<div class="card"><img
+                    src="${dolar.img}">
+                <div class="coin-data">
+                    <p>${dolar.screenText}</p>
+                    <p> <span class="legend"> Compra: </span> $${dolar.buy}</p>
+                    <p> <span class="legend"> Venta: </span> $${dolar.sell}</p>
+                </div>
+            </div>
+            `
+}
+
+function renderDolar(dolar) {
+    const dolarHtml = getDolarHtml(dolar)
+    cardContainer.insertAdjacentHTML("beforeend", dolarHtml);
+}
